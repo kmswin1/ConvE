ae6f1afb2a4ccf8ade407e399ac0ea8547b81005
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-01-20 13:05:28 +0900

    add prediction logic

---
+++ pred_evaluation.py
@@ -0,0 +1,112 @@
+import torch
+import numpy as np
+import datetime
+from logger import get_logger
+from utils import batch_by_size
+import time
+#timer = CUDATimer()
+logger = get_logger('eval', True, True, 'prediction.txt')
+
+# ranking_and_hits(model, Config.batch_size, valid_data, eval_h, eval_t,'dev_evaluation', kg_vocab)
+def ranking_and_hits(model, batch_size, dateset, eval_h, eval_t, name, kg_vocab):
+    heads, rels, tails = dateset
+    logger.info('')
+    logger.info('-'*50)
+    logger.info(name)
+    logger.info('-'*50)
+    logger.info('')
+    hits_left = []
+    hits_right = []
+    hits = []
+    ranks = []
+    ranks_left = []
+    ranks_right = []
+
+    for i in range(10):
+        hits_left.append([])
+        hits_right.append([])
+        hits.append([])
+
+    for bh, br, bt in batch_by_size(batch_size, heads, rels, tails):
+        b_size = bh.size(0)
+        bh = bh.cuda();br = br.cuda();bt = bt.cuda()
+        pred1 = model.forward(bh, br)
+        pred2 = model.forward(bt, br)
+
+        e2_multi1 = torch.empty(b_size, pred1.size(1))
+        e2_multi2 = torch.empty(b_size, pred1.size(1))
+
+        for i, (h, r, t) in enumerate(zip(bh, br, bt)):
+            e2_multi1[i] = eval_t[h.item(), r.item()].to_dense()
+            e2_multi2[i] = eval_h[t.item(), r.item()].to_dense()
+        e2_multi1 = e2_multi1.cuda()
+        e2_multi2 = e2_multi2.cuda()
+
+        for i in range(b_size):
+            # save the prediction that is relevant
+            target_value1 = pred1[i,bt[i].item()].item()
+            target_value2 = pred2[i,bh[i].item()].item()
+            # zero all known cases (this are not interesting)
+            # this corresponds to the filtered setting
+            pred1[i] += e2_multi1[i] * (-1e20)
+            pred2[i] += e2_multi2[i] * (-1e20)
+            # write base the saved values
+            pred1[i][bt[i].item()] = target_value1
+            pred2[i][bh[i].item()] = target_value2
+
+        # sort and rank
+        max_values, argsort1 = torch.sort(pred1, 1, descending=True)
+        max_values, argsort2 = torch.sort(pred2, 1, descending=True)
+        # max_values, argsort2 = torch.sort(pred2, 1, descending=True)
+        for i in range(b_size):
+            # find the rank of the target entities
+            find_target1 = argsort1[i] == bt[i]
+            find_target2 = argsort2[i] == bh[i]
+            rank1 = torch.nonzero(find_target1)[0, 0].item() + 1
+            rank2 = torch.nonzero(find_target2)[0, 0].item() + 1
+            # rank+1, since the lowest rank is rank 1 not rank 0
+            # ranks.append(rank1+1)
+            ranks_left.append(rank1)
+            # ranks.append(rank2+1)
+            ranks_right.append(rank2)
+
+            # this could be done more elegantly, but here you go
+            hits[9].append(int(rank1<=10))
+            hits[9].append(int(rank2<=10))
+            hits_left[9].append((int(rank1<=10)))
+            hits_right[9].append((int(rank2<=10)))
+            # for hits_level in range(10):
+            #     if rank1 <= hits_level:
+            #         hits[hits_level].append(1.0)
+            #         hits_left[hits_level].append(1.0)
+            #     else:
+            #         hits[hits_level].append(0.0)
+            #         hits_left[hits_level].append(0.0)
+            #
+            #     if rank2 <= hits_level:
+            #         hits[hits_level].append(1.0)
+            #         hits_right[hits_level].append(1.0)
+            #     else:
+            #         hits[hits_level].append(0.0)
+            #         hits_right[hits_level].append(0.0)
+
+    # for i in range(10):
+    #     logger.info('Hits left @{0}: {1}'.format(i+1, np.mean(hits_left[i])))
+    #     logger.info('Hits right @{0}: {1}'.format(i+1, np.mean(hits_right[i])))
+    logger.info ('head : ' + kg_vocab.entlist[h])
+    logger.info("predicted tails : ")
+    for i in argsort1:
+        logger.info(kg_vocab.entlist[i])
+    logger.info ('tail : ' + kg_vocab.entlist[t])
+    for i in argsort2:
+        logger.info(kg_vocab.entlist[i])
+    logger.info("predicted heads : ")
+    logger.info('Hits tail @{0}: {1}'.format(10, np.mean(hits_left[9])))
+    logger.info('Hits head @{0}: {1}'.format(10, np.mean(hits_right[9])))
+    logger.info('Hits @{0}: {1}'.format(10, np.mean(hits[9])))
+    logger.info('Mean rank tail: {0}'.format(np.mean(ranks_left)))
+    logger.info('Mean rank head: {0}'.format(np.mean(ranks_right)))
+    logger.info('Mean rank: {0}'.format(np.mean(ranks_left+ranks_right)))
+    logger.info('Mean reciprocal rank tail: {0}'.format(np.mean(1./np.array(ranks_left))))
+    logger.info('Mean reciprocal rank head: {0}'.format(np.mean(1./np.array(ranks_right))))
+    logger.info('Mean reciprocal rank: {0}'.format(np.mean(1./np.array(ranks_left+ranks_right))))
---
+++ predict_main.py
@@ -0,0 +1,79 @@
+# !coding=utf8
+import json
+import torch
+import argparse
+import os
+from utils import heads_tails, inplace_shuffle, batch_by_num, batch_by_size, make_kg_vocab, graph_size, read_data, read_reverse_data, read_data_with_rel_reverse
+import logging
+import time, datetime
+from pred_evaluation import ranking_and_hits
+
+dir = os.getcwd() + '/data'
+
+
+def main(args, model_path):
+    print (os.getcwd())
+    print ('start prediction ...')
+
+    train_data = dir + '/train.json'
+    #valid_data = dir + '/valid.json'
+    test_data = dir + '/test.json'
+
+    kg_vocab = make_kg_vocab(train_data, test_data)
+    n_ent, n_rel = graph_size(kg_vocab)
+
+    train_data_with_reverse = read_data_with_rel_reverse(os.path.join(dir, 'train.json'), kg_vocab)
+    inplace_shuffle(*train_data_with_reverse)
+    heads, tails = heads_tails(n_ent, train_data_with_reverse)
+
+    train_data = read_data(os.path.join(dir, 'train.json'), kg_vocab)
+    #valid_data = read_data(os.path.join(dir, 'valid.json'), kg_vocab)
+    test_data = read_data(os.path.join(dir, 'test.json'), kg_vocab)
+    eval_h, eval_t = heads_tails(n_ent, train_data, test_data)
+
+    #valid_data = [torch.LongTensor(vec) for vec in valid_data]
+    test_data = [torch.LongTensor(vec) for vec in test_data]
+    train_data_with_reverse = [torch.LongTensor(vec) for vec in train_data_with_reverse]
+
+    model = torch.load_state_dict(torch.load('/root/person_conve_0.2_0.3.model'))
+    print ('cuda : ' + str(torch.cuda.is_available()))
+
+    model.eval()
+    with torch.no_grad():
+        start = time.time()
+        ranking_and_hits(model, args.batch_size, test_data, eval_h, eval_t,'dev_evaluation', kg_vocab)
+        end = time.time()
+        logging.info('eval time used: {} minutes'.format((end - start)/60))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='KG completion for cruise contents data')
+    parser.add_argument('--batch-size', type=int, default=128, help='input batch size for training (default: 128)')
+    parser.add_argument('--test-batch-size', type=int, default=128, help='input batch size for testing/validation (default: 128)')
+    parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train (default: 1000)')
+    parser.add_argument('--lr', type=float, default=0.003, help='learning rate (default: 0.003)')
+    parser.add_argument('--seed', type=int, default=17, metavar='S', help='random seed (default: 17)')
+    parser.add_argument('--log-interval', type=int, default=100, help='how many batches to wait before logging training status')
+    parser.add_argument('--data', type=str, default='entire', help='The kind of domain for training cruise data, default: person')
+    parser.add_argument('--l2', type=float, default=0.0, help='Weight decay value to use in the optimizer. Default: 0.0')
+    parser.add_argument('--model', type=str, default='conve', help='Choose from: {conve, distmult, complex}')
+    parser.add_argument('--embedding-dim', type=int, default=200, help='The embedding dimension (1D). Default: 200')
+    parser.add_argument('--embedding-shape1', type=int, default=20, help='The first dimension of the reshaped 2D embedding. The second dimension is infered. Default: 20')
+    parser.add_argument('--hidden-drop', type=float, default=0.3, help='Dropout for the hidden layer. Default: 0.3.')
+    parser.add_argument('--input-drop', type=float, default=0.2, help='Dropout for the input embeddings. Default: 0.2.')
+    parser.add_argument('--feat-drop', type=float, default=0.2, help='Dropout for the convolutional features. Default: 0.2.')
+    parser.add_argument('--lr-decay', type=float, default=0.995, help='Decay the learning rate by this factor every epoch. Default: 0.995')
+    parser.add_argument('--loader-threads', type=int, default=4, help='How many loader threads to use for the batch loaders. Default: 4')
+    parser.add_argument('--use-bias', action='store_true', help='Use a bias in the convolutional layer. Default: True')
+    parser.add_argument('--label-smoothing', type=float, default=0.1, help='Label smoothing value to use. Default: 0.1')
+    parser.add_argument('--hidden-size', type=int, default=9728, help='The side of the hidden layer. The required size changes with the size of the embeddings. Default: 9728 (embedding size 200).')
+    parser.add_argument('--log-file', action='store', type=str)
+
+    args = parser.parse_args()
+
+
+    model_name = '{2}_{0}_{1}'.format(args.input_drop, args.hidden_drop, args.model)
+    model_path = 'saved_models/{0}_{1}.model'.format(args.data, model_name)
+
+    torch.manual_seed(args.seed)
+    main(args, model_path)
