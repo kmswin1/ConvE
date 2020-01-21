5a343906fcd70622714aa84e255521c95444a5c6
Minsang Kim <kmswin1@korea.ac.kr>
2020-01-21 22:00:10 +0900

    debugging

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main(args, model_path):
     n_ent, n_rel = graph_size(kg_vocab)
 
     train_data_with_reverse = read_data_with_rel_reverse(os.path.join(dir, 'train.json'), kg_vocab)
+    test_data_with_reverse = read_data_with_rel_reverse(os.path.join(dir, 'test.json'), kg_vocab)
     inplace_shuffle(*train_data_with_reverse)
     heads, tails = heads_tails(n_ent, train_data_with_reverse)
 
@@ -32,7 +33,7 @@ def main(args, model_path):
     #valid_data = read_data(os.path.join(dir, 'valid.json'), kg_vocab)
     test_data = read_data(os.path.join(dir, 'test.json'), kg_vocab)
     test_reverse = read_reverse_data(os.path.join(dir, 'test.json'), kg_vocab)
-    eval_h, eval_t = heads_tails(n_ent, train_data, test_data)
+    eval_h, eval_t = heads_tails(n_ent, train_data_with_reverse, test_data_with_reverse)
 
     #valid_data = [torch.LongTensor(vec) for vec in valid_data]
     test_data = [torch.LongTensor(vec) for vec in test_data]
