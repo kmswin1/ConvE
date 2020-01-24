648a3a842065cb76048b47860b13c7897825bcda
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-01-25 00:45:14 +0900

    change notify

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main(args, model_path):
     test_data = dir + '/test.json'
 
     kg_vocab = make_kg_vocab(train_data, test_data)
-    print ("making vocab is finish")
+    print ("making vocab is done")
     n_ent, n_rel = graph_size(kg_vocab)
 
     train_data_with_reverse = read_data_with_rel_reverse(os.path.join(dir, 'train.json'), kg_vocab)
@@ -28,14 +28,14 @@ def main(args, model_path):
     train_reverse = read_reverse_data(os.path.join(dir, 'train.json'), kg_vocab)
     inplace_shuffle(*train_data_with_reverse)
     heads, tails = heads_tails(n_ent, train_data_with_reverse)
-    print ("making read train data and make train heads, tails is finish")
+    print ("making read train data and make train heads, tails is done")
 
 
     #valid_data = read_data(os.path.join(dir, 'valid.json'), kg_vocab)
     test_data = read_data(os.path.join(dir, 'test.json'), kg_vocab)
     test_reverse = read_reverse_data(os.path.join(dir, 'test.json'), kg_vocab)
     eval_h, eval_t = heads_tails_eval(n_ent, train_data, train_reverse, test_data, test_reverse)
-    print ("making read test data and make test heads, tails is finish")
+    print ("making read test data and make test heads, tails is done")
 
 
     #valid_data = [torch.LongTensor(vec) for vec in valid_data]
