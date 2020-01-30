5ee1d5b2685b55d504e6658a469e98c41f846dbd
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-01-30 10:32:22 +0900

    debugging

--- evaluation.py
+++ evaluation.py
@@ -34,14 +34,12 @@ def ranking_and_hits(model, args, testset, n_ent, epoch):
         batch_size = head.size(0)
 
         start = time.time()
-        e2_multi1 = torch.zeros(args.batch_size, n_ent)
-        e2_multi2 = torch.zeros(args.batch_size, n_ent)
+        e2_multi1 = torch.zeros(args.batch_size, n_ent, dtype=torch.int32)
+        e2_multi2 = torch.zeros(args.batch_size, n_ent, dtype=torch.int32)
         for i, (t,t_r) in enumerate(zip(tail, tail2)):
             e2_multi1[i][t] = 1
             e2_multi2[i][t_r] = 1
         print("e2_multi_time " + str(time.time() - start))
-        e2_multi1 = torch.LongTensor(e2_multi1)
-        e2_multi2 = torch.LongTensor(e2_multi2)
         e2_multi1 = e2_multi1.cuda()
         e2_multi2 = e2_multi2.cuda()
         pred1 = model.forward(head, rel)
