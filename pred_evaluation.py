8d6f7e31529b967b2c933aecee9522cbd761d305
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-01-31 13:09:34 +0900

    debugging

--- pred_evaluation.py
+++ pred_evaluation.py
@@ -118,13 +118,13 @@ def ranking_and_hits(model, args, testset, n_ent, kg_vocab, epoch):
             if rank2 == 1:
                 with open(dir + '/log_file/hit.txt', 'a') as f:
                     f.write(kg_vocab.ent_list[head2[i].item()]+"\n")
-                    f.write(kg_vocab.rel_rev_list[rel_rev[i].item()]+"\n")
+                    f.write(kg_vocab.rel_list[rel_rev[i].item()]+"\n")
                     f.write(kg_vocab.ent_list[argsort2[i][0].item()]+"\n")
                     f.close()
             elif rank2 > 1:
                 with open(dir + '/log_file/nohit.txt', 'a') as f:
                     f.write(kg_vocab.ent_list[head2[i].item()]+"\n")
-                    f.write(kg_vocab.rel_rev_list[rel_rev[i].item()]+"\n")
+                    f.write(kg_vocab.rel_list[rel_rev[i].item()]+"\n")
                     f.write(kg_vocab.ent_list[argsort2[i][0].item()]+"\n")
                     f.close()
 
