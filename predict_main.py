f2f20dc010126c94b8c515e8bf81021b7e412198
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-01-31 13:06:50 +0900

    debugging

--- predict_main.py
+++ predict_main.py
@@ -55,10 +55,10 @@ def main(args, model_path):
     model = ConvE(args, n_ent, n_rel)
     model.init()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.load_state_dict(torch.load(model_path))
     if torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
         criterion = torch.nn.BCELoss()
+    model.load_state_dict(torch.load(model_path))
     model.cuda()
     print ('cuda : ' + str(torch.cuda.is_available()) + ' count : ' + str(torch.cuda.device_count()))
 
