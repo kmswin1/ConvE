b07e646a69cc8a48cd1739b13564a2628087f0ec
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-02-03 10:31:27 +0900

    debugging

--- preprocessing.py
+++ preprocessing.py
@@ -93,7 +93,7 @@ def write_training_graph(cases, graph, path, kg_vocab):
 
             # (John, fatherOf) -> Tom
             # (John, fatherOf_reverse, Mike)
-            entities1 = "@@".join(list(graph[key]))
+            entities1 = "@@".join(list(str(graph[key])))
 
             data_point = {}
             data_point['e1'] = e1
@@ -114,8 +114,8 @@ def write_evaluation_graph(cases, graph, path, kg_vocab):
             # (Mike, fatherOf) -> John
             # (John, fatherOf, Tom)
             rel_reverse = rel+'_reverse'
-            entities1 = "@@".join(list(graph[(e1, rel)]))
-            entities2 = "@@".join(list(graph[(e2, rel_reverse)]))
+            entities1 = "@@".join(list(str(graph[(e1, rel)])))
+            entities2 = "@@".join(list(str(graph[(e2, rel_reverse)])))
 
             n1 += len(entities1.split('@@'))
             n2 += len(entities2.split('@@'))
