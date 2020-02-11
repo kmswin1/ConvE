a31484a0476beff0d78b836701b8a95ca7e283a6
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-02-11 14:33:29 +0900

    debugging

--- model.py
+++ model.py
@@ -92,9 +92,7 @@ class ConvE(torch.nn.Module):
         x = self.bn2(x)
         x = F.relu(x)
         x = x.view(-1, 1, self.emb_dim)
-        y = self.emb_e(e2).transpose(2,1)
-        y = y.transpose(1,0)
-        y = y.transpose(2,1)
+        y = self.emb_e(e2)
 
         x = self.softmax()
 
