e1193c537207a3538dcd1c7da7c60856aede686a
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-02-12 18:40:22 +0900

    del rev in eval

--- datasets.py
+++ datasets.py
@@ -37,30 +37,31 @@ class KG_DataSet(Dataset):
 
 class KG_EvalSet(Dataset):
     def __init__(self, file_path, args, n_ent):
+        self.n_ent = n_ent
+        self.args = args
         self.len = 0
         self.head = []
         self.rel = []
-        self.tail = []
-        self.head2 = []
-        self.rel_rev = []
-        self.tail2 = []
-        self.n_ent = n_ent
+        self.triple = {}
         with open(file_path) as f:
             for line in f:
+                #line = json.loads(line)
                 self.len += 1
-                line = json.loads(line)
-                self.head.append(line['e1'])
-                self.rel.append(line['rel'])
-                self.tail.append(line['e2_e1toe2'])
-                self.head2.append(line['e2'])
-                self.rel_rev.append(line['rel_eval'])
-                self.tail2.append(line['e1_e2toe1'])
+                line = line.strip("\n")
+                line = line.split(' ')
+                self.head.append(int(line[0]))
+                self.rel.append(int(line[1]))
+                if (line[0], line[1]) not in self.triple.keys():
+                    self.triple[line[0], line[1]] = set()
+
+                self.triple[line[0],line[1]].add(int(line[2]))
+
     def __len__(self):
         return self.len
 
     def __getitem__(self, idx):
+        tail = list(self.triple[str(self.head[idx]), str(self.rel[idx])])
         logits = torch.zeros(self.n_ent)
-        logits[self.tail[idx]] = 1
-        logits2 = torch.zeros(self.n_ent)
-        logits2[self.tail2[idx]] = 1
-        return self.head[idx], self.rel[idx], logits, self.head2[idx], self.rel_rev[idx], logits2
+        logits[tail] = 1
+
+        return self.head[idx], self.rel[idx], logits
--- evaluation.py
+++ evaluation.py
@@ -17,11 +17,12 @@ def ranking_and_hits(model, args, evalloader, n_ent, epoch):
         hits.append([])
 
     for i, data in enumerate(evalloader):
-        head, rel, tail, head2, rel_rev, tail2 = data
+        #head, rel, tail, head2, rel_rev, tail2 = data
+        head, rel, tail = data
         head = torch.LongTensor(head)
         rel = torch.LongTensor(rel)
-        head2 = torch.LongTensor(head2)
-        rel_rev = torch.LongTensor(rel_rev)
+        #head2 = torch.LongTensor(head2)
+        #rel_rev = torch.LongTensor(rel_rev)
         head = head.cuda()
         rel = rel.cuda()
         head2 = head2.cuda()
@@ -29,42 +30,42 @@ def ranking_and_hits(model, args, evalloader, n_ent, epoch):
         batch_size = head.size(0)
 
         e2_multi1 = tail.cuda()
-        e2_multi2 = tail2.cuda()
+        #e2_multi2 = tail2.cuda()
         pred1 = model.forward(head, rel)
-        pred2 = model.forward(head2, rel_rev)
+        #pred2 = model.forward(head2, rel_rev)
 
         for i in range(batch_size):
             # save the prediction that is relevant
             target_value1 = pred1[i][head2[i]].item()
-            target_value2 = pred2[i][head[i]].item()
+            #target_value2 = pred2[i][head[i]].item()
             # zero all known cases (this are not interesting)
             # this corresponds to the filtered setting
             pred1[i][e2_multi1[i] == 1] = 0.0
-            pred2[i][e2_multi2[i] == 1] = 0.0
+            #pred2[i][e2_multi2[i] == 1] = 0.0
             # write base the saved values
             pred1[i][head2[i]] = target_value1
-            pred2[i][head[i]] = target_value2
+            #pred2[i][head[i]] = target_value2
 
         # sort and rank
         max_values, argsort1 = torch.sort(pred1, 1, descending=True)
-        max_values, argsort2 = torch.sort(pred2, 1, descending=True)
+        #max_values, argsort2 = torch.sort(pred2, 1, descending=True)
         for i in range(batch_size):
             # find the rank of the target entities
             find_target1 = argsort1[i] == head2[i]
-            find_target2 = argsort2[i] == head[i]
+            #find_target2 = argsort2[i] == head[i]
             rank1 = torch.nonzero(find_target1)[0, 0].item() + 1
-            rank2 = torch.nonzero(find_target2)[0, 0].item() + 1
+            #rank2 = torch.nonzero(find_target2)[0, 0].item() + 1
             # rank+1, since the lowest rank is rank 1 not rank 0
             ranks.append(rank1+1)
             ranks_left.append(rank1)
-            ranks.append(rank2+1)
-            ranks_right.append(rank2)
+            #ranks.append(rank2+1)
+            #ranks_right.append(rank2)
 
             # this could be done more elegantly, but here you go
             hits[9].append(int(rank1<=10))
-            hits[9].append(int(rank2<=10))
+            #hits[9].append(int(rank2<=10))
             hits_left[9].append((int(rank1<=10)))
-            hits_right[9].append((int(rank2<=10)))
+            #hits_right[9].append((int(rank2<=10)))
             # for hits_level in range(10):
             #     if rank1 <= hits_level:
             #         hits[hits_level].append(1.0)
@@ -85,23 +86,23 @@ def ranking_and_hits(model, args, evalloader, n_ent, epoch):
     #     logger.info('Hits right @{0}: {1}'.format(i+1, np.mean(hits_right[i])))
 
     print('Hits tail @{0}: {1}'.format(10, np.mean(hits_left[9])))
-    print('Hits head @{0}: {1}'.format(10, np.mean(hits_right[9])))
+    #print('Hits head @{0}: {1}'.format(10, np.mean(hits_right[9])))
     print('Hits @{0}: {1}'.format(10, np.mean(hits[9])))
     print('Mean rank tail: {0}'.format(np.mean(ranks_left)))
-    print('Mean rank head: {0}'.format(np.mean(ranks_right)))
+    #print('Mean rank head: {0}'.format(np.mean(ranks_right)))
     print('Mean rank: {0}'.format(np.mean(ranks_left+ranks_right)))
     print('Mean reciprocal rank tail: {0}'.format(np.mean(1./np.array(ranks_left))))
-    print('Mean reciprocal rank head: {0}'.format(np.mean(1./np.array(ranks_right))))
+    #print('Mean reciprocal rank head: {0}'.format(np.mean(1./np.array(ranks_right))))
     print('Mean reciprocal rank: {0}'.format(np.mean(1./np.array(ranks_left+ranks_right))))
 
     with open(dir+'/log_file/log.txt', 'a') as f:
         f.write(str(epoch) + '-----evaluation-----\n')
         f.write('Hits tail @{0}: {1}\n'.format(10, np.mean(hits_left[9])))
-        f.write('Hits head @{0}: {1}\n'.format(10, np.mean(hits_right[9])))
+        #f.write('Hits head @{0}: {1}\n'.format(10, np.mean(hits_right[9])))
         f.write('Hits @{0}: {1}\n'.format(10, np.mean(hits[9])))
         f.write('Mean rank tail: {0}\n'.format(np.mean(ranks_left)))
-        f.write('Mean rank head: {0}\n'.format(np.mean(ranks_right)))
+        #f.write('Mean rank head: {0}\n'.format(np.mean(ranks_right)))
         f.write('Mean rank: {0}\n'.format(np.mean(ranks_left+ranks_right)))
         f.write('Mean reciprocal rank tail: {0}\n'.format(np.mean(1./np.array(ranks_left))))
-        f.write('Mean reciprocal rank head: {0}\n'.format(np.mean(1./np.array(ranks_right))))
+        #f.write('Mean reciprocal rank head: {0}\n'.format(np.mean(1./np.array(ranks_right))))
         f.write('Mean reciprocal rank: {0}\n'.format(np.mean(1./np.array(ranks_left+ranks_right))))
