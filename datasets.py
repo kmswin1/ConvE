431adb3fe842e8a497e67864a79042538870c2a4
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-02-12 13:46:46 +0900

    debugging

--- datasets.py
+++ datasets.py
@@ -9,34 +9,35 @@ class KG_DataSet(Dataset):
         self.len = 0
         self.head = []
         self.rel = []
-        self.tail = []
         self.smoothed_value = 1 - args.label_smoothing
         self.epsilon = 1.0/self.n_ent
         self.sample_num = args.sample_num
+        self.triple = {}
         with open(file_path) as f:
             for line in f:
-                self.len += 1
                 #line = json.loads(line)
                 line.strip("\n")
                 line = line.split(' ')
-                self.head.append(int(line[0]))
-                self.rel.append(int(line[1]))
-                self.tail.append(int(line[2]))
+                self.head.append(line[0])
+                self.rel.append(line[1])
+                if (line[0], line[1]) not in self.triple.keys():
+                    self.triple[line[0], line[1]] = set()
+
+                self.triple[line[0],line[1]].add(line[2])
+
 
         self.head = torch.LongTensor(self.head)
         self.rel = torch.LongTensor(self.rel)
-        self.tail = torch.LongTensor(self.tail)
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, idx):
-        #logits = torch.full((self.n_ent, ), self.epsilon)
-        #logits[self.tail[idx]] = self.smoothed_value
-        perm = torch.randperm(self.n_ent)
+        self.tail = list(self.triple[self.head[idx], self.rel[idx]])
+        logits = torch.full((self.n_ent, ), self.epsilon)
+        logits[self.tail] = self.smoothed_value
 
-        sample = perm[perm != self.tail[idx]][:self.sample_num]
-        return self.head[idx], self.rel[idx], self.tail[idx], sample
+        return self.head[idx], self.rel[idx], self.tail[idx]
 
 class KG_EvalSet(Dataset):
     def __init__(self, file_path, args, n_ent):
