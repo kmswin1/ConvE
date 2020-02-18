0808dbfbdbb7aa98084e92efa741088862b61d4f
lucas.ai <lucas.ai@kakaoenterprise.com>
2020-02-18 11:07:27 +0900

    d

--- predict_main.py
+++ predict_main.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('--use-bias', action='store_true', help='Use a bias in the convolutional layer. Default: True')
     parser.add_argument('--label-smoothing', type=float, default=0.1, help='Label smoothing value to use. Default: 0.1')
     parser.add_argument('--multi-gpu', type=bool, default=False, help='choose the training using by multigpu')
+    parser.add_argument('--feature-channel', type=int, default=32, help='The number of Feature map channels. Default: 32')
 
     args = parser.parse_args()
 
